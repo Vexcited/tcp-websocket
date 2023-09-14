e010e993d4463cb3bdcc4a9d604e03dbdd2c9087
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-14 19:18:30 +0200

    feat: implement `Message` class

---
+++ src/websocket/message.ts
@@ -0,0 +1,31 @@
+import type Frame from "./frame";
+
+class Message {
+  public rsv1 = false;
+  public rsv2 = false;
+  public rsv3 = false;
+  public opcode: number | null = null;
+  public length = 0;
+  public data: Buffer | undefined;
+  #chunks: Buffer[] = [];
+  
+  read () {
+    this.data = this.data ?? Buffer.concat(this.#chunks, this.length);
+    return this.data;
+  }
+  
+  pushFrame (frame: Frame) {
+    this.rsv1 = this.rsv1 ?? frame.rsv1;
+    this.rsv2 = this.rsv2 ?? frame.rsv2;
+    this.rsv3 = this.rsv3 ?? frame.rsv3;
+  
+    if (typeof this.opcode !== "number") {
+      this.opcode = frame.opcode;
+    }
+  
+    this.#chunks.push(frame.payload!);
+    this.length += frame.length;
+  }
+};
+
+export default Message;

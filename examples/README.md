e7a02aa95b047690087f4f194c4a2d566e8be595
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-15 17:21:48 +0200

    chore(examples): add proper tsconfig

---
+++ examples/README.md
@@ -0,0 +1,7 @@
+# Examples - `tcp-websocket` 
+
+Small and easy to understand examples to quickly get started with `tcp-websocket`.
+
+## Listing
+
+- [`simple.ts`](./simple.ts)
--- examples/simple.ts
+++ examples/simple.ts
@@ -1,14 +1,23 @@
-/** Replace "../src" with "tcp-websocket". */
+// Outside of this repository, replace "../src" with "tcp-websocket".
 import TCPWebSocket from "../src";
 
-const ws = new TCPWebSocket("wss://echo-websocket.hoppscotch.io", {
-  headers: { Origin: "https://hoppscotch.io" }
-});
+const ws = new TCPWebSocket("wss://ws.postman-echo.com/raw");
 
 ws.on("open", () => {
-  console.info("[open]: Connected !");
+  console.info("[open]: connected !\n");
+  
+  const data = "hello world!";
+  ws.send(data);
+  console.info("[send::message]:", data);
 });
 
-ws.on("message", (event) => {
-  console.info("[message]:", event.data);
+ws.once("message", (event) => {
+  console.info("[receive::message]:", event.data);
+
+  console.info("\n[info]: will close in 5 seconds...");
+  setTimeout(() => ws.close(), 5_000)
+});
+
+ws.on("close", (event) => {
+  console.info(`[close(${event.code})]: ${event.reason || "[no reason provided]"}`)
 });
